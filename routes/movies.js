112a65bbbb53c9d78b1fcc6c6f1df62f50bd3308
CreativeMarkus <mark@creativemarkus.com>
2025-05-24 11:03:46 -0400

    fixed the movies.js file

--- routes/movies.js
+++ routes/movies.js
@@ -6,3 +6,16 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
+
+
+// Get all movies
+app.get("/movies", (req, res) => {
+  res.json(movies);
+});
+
+// Get a movie by title
+app.get("/movies/:title", (req, res) => {
+  const movie = movies.find(m => m.title.toLowerCase() === req.params.title.toLowerCase());
+  if (movie) res.json(movie);
+  else res.status(404).send("Movie not found.");
+});
