4a942558310de3b7fbe45c3d1ad62ef9d182d551
CreativeMarkus <mark@creativemarkus.com>
2025-06-10 10:52:03 -0400

    update server and movies.js

--- server.js
+++ server.js
@@ -4,23 +4,19 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json());
 app.use(express.static('public'));
 
-// Route imports
 const moviesRoutes = require('./routes/movies');
 const usersRoutes = require('./routes/users');
 const genresRoutes = require('./routes/genres');
 const directorsRoutes = require('./routes/directors');
 
-// Mount routes
 app.use('/movies', moviesRoutes);
 app.use('/users', usersRoutes);
 app.use('/genres', genresRoutes);
 app.use('/directors', directorsRoutes);
 
-// Serve static HTML pages
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -29,21 +25,15 @@ app.get('/documentation', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'documentation.html'));
 });
 
-// 404 handler (should come after all other routes)
 app.use((req, res) => {
   res.status(404).send('404 - Not Found');
 });
 
-// Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
 
-app.use("/users", usersRoutes);
-app.use("/movies", moviesRoutes);
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
