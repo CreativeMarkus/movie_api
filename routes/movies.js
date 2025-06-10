c35012ec3d1b7a4d22cb8b663b3e0d3c525ff8d8
CreativeMarkus <mark@creativemarkus.com>
2025-06-10 10:20:37 -0400

    update movie.js

--- routes/movies.js
+++ routes/movies.js
@@ -20,7 +20,7 @@ const movies = [
 
 router.get("/directors/:name", (req, res) => {
   const name = decodeURIComponent(req.params.name); 
-  const movie = movies.find(m => m.director === name);
+ const movie = movies.find(m => m.director.toLowerCase() === name.toLowerCase());
   if (movie) {
     res.json({ director: movie.director });
   } else {
