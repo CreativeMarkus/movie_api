92c7b1dca88c852a91eb6294a4d637fab233326d
CreativeMarkus <mark@creativemarkus.com>
2025-06-17 15:42:09 -0400

    updated users.js

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,28 @@ const router = express.Router();
 const Models = require('../models');
 
 
+app.get('/users', async (req, res) => {
+  await Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+app.get('/users/:Username', async (req, res) => {
+  await Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 
 router.get("/", async (req, res) => {
   try {
