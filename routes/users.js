34de6739274a3ca32cac1c73029e05a257df5f06
CreativeMarkus <mark@creativemarkus.com>
2025-06-17 15:21:08 -0400

    updated the models

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { User } = require("models"); 
+const Models = require('models');
+
+
 
 router.get("/", async (req, res) => {
   try {
