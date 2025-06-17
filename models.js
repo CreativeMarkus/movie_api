20ade99f64cd5a8ee4588b09b984a47b0dd15b98
CreativeMarkus <mark@creativemarkus.com>
2025-06-17 13:58:51 -0400

    updates

--- models.js
+++ models.js
@@ -1,30 +1,29 @@
-const mongoose = require('mongoose');
-let movieSchema = mangoose.Scema({
-    Title: {Type: String, required: True},
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
     Description: {type: String, required: true},
     Genre: {
         Name: String,
-        Descrition: String
+        Description: String
     },
-    Director: {
-        Name: String,
-        Bio: String
-    },
-    Actors: [String],
-    ImagePath: String,
-    Featured: Boolean
+Director: {
+    Name: String,
+    Bio: String
+},
+Actors: [String],
+ImagePath: String,
+Featured: Boolean
 });
 
-let userSchema = mangoose.Schema({
-    Username: {type: String, required: true,},
-Password: { type: String, required: true},
-Email: {type: String, required: true},
-Birthday: Date,
-FavoriteMovies: [{ type: mangoose.Schema.Types.ObjectID, ref: 'Movies'}]
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Type.ObjectId, ref: 'Movie' }]
 });
 
-let  Movie = mangoose.model('Movie',movieSchema);
-let user = mangoose.model ('User', userSchema);
+let Movie = mongoose.model('Movie' , movieSchema);
+let User = mongoose.model('User' , userSchema);
 
 module.exports.Movie = Movie;
-module.exports.User = this.User;
+module.exports.User = User;
