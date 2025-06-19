7063e79569f774a5bd902d0d020128b7b4ad875d
CreativeMarkus <mark@creativemarkus.com>
2025-06-19 11:32:12 -0400

    updated model and server

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const usersRoutes = require('./routes/users');
 
 const app = express();
-app.use(express.json());
-app.use('/users', usersRoutes);
-
 const PORT = process.env.PORT || 3000;
 const mongoURI = 'mongodb://localhost:27017/movie_api';
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 
@@ -17,6 +17,14 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 db.once('open', () => {
   console.log('Connected to MongoDB');
+
+  const Models = require('./models.js');
+  const Movies = Models.Movie;
+  const Users = Models.User;
+
+  app.use(express.json());
+
+  app.use('/users', usersRoutes);
 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
