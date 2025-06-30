76523fbfa2309b892bc8572166c465e323043c80
CreativeMarkus <mark@creativemarkus.com>
2025-06-30 14:54:34 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,121 +1,47 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require("express-validator");
 const passport = require("passport");
 const { User } = require("../models.js");
-check('Username', 'Username contains non-alphanumeric characters - not allowed.').isAlphanumeric()
 
-app.post('/users',
-
+router.post(
+  "/users",
   [
-    check('Username', 'Username is required').isLength({min: 5}),
-    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-    check('Password', 'Password is required').not().isEmpty(),
-    check('Email', 'Email does not appear to be valid').isEmail()
-  ], async (req, res) => {
-
-    let errors = validationResult(req);
+    check("Username", "Username is required").isLength({ min: 5 }),
+    check("Username", "Username contains non-alphanumeric characters - not allowed.").isAlphanumeric(),
+    check("Password", "Password is required").not().isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
 
-    let hashedPassword = Users.hashPassword(req.body.Password);
-    await Users.findOne({ Username: req.body.Username }) 
-      .then((user) => {
-        if (user) {
-          return res.status(400).send(req.body.Username + ' already exists');
-        } else {
-          Users
-            .create({
-              Username: req.body.Username,
-              Password: hashedPassword,
-              Email: req.body.Email,
-              Birthday: req.body.Birthday
-            })
-            .then((user) => { res.status(201).json(user) })
-            .catch((error) => {
-              console.error(error);
-              res.status(500).send('Error: ' + error);
-            });
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send('Error: ' + error);
+    const hashedPassword = User.hashPassword(req.body.Password);
+
+    try {
+      const existingUser = await User.findOne({ Username: req.body.Username });
+
+      if (existingUser) {
+        return res.status(400).send(req.body.Username + " already exists");
+      }
+
+      User.create({
+        Username: req.body.Username,
+        Password: hashedPassword,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
       });
-  });
 
-router.put(
-  "/:Username",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    if (req.user.Username !== req.params.Username) {
-      return res.status(400).send("Permission denied");
-    }
-
-    try {
-      const updatedUser = await User.findOneAndUpdate(
-        { Username: req.params.Username },
-        { $set: req.body },
-        { new: true }
-      );
-      res.json(updatedUser);
-    } catch (err) {
-      res.status(500).send("Error: " + err);
+      res.status(201).json(newUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error: " + error);
     }
   }
 );
 
-router.post(
-  "/:Username/movies/:MovieID",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    try {
-      const updatedUser = await User.findOneAndUpdate(
-        { Username: req.params.Username },
-        { $push: { FavoriteMovies: req.params.MovieID } },
-        { new: true }
-      );
-      res.json(updatedUser);
-    } catch (err) {
-      res.status(500).send(err);
-    }
-  }
-);
-
-router.delete(
-  "/:Username/movies/:MovieID",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    try {
-      const updatedUser = await User.findOneAndUpdate(
-        { Username: req.params.Username },
-        { $pull: { FavoriteMovies: req.params.MovieID } },
-        { new: true }
-      );
-      res.json(updatedUser);
-    } catch (err) {
-      res.status(500).send(err);
-    }
-  }
-);
-
-router.delete(
-  "/:Username",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    if (req.user.Username !== req.params.Username) {
-      return res.status(400).send("Permission denied");
-    }
-
-    try {
-      await User.findOneAndRemove({ Username: req.params.Username });
-      res.status(200).send("User deleted");
-    } catch (err) {
-      res.status(500).send("Error: " + err);
-    }
-  }
-);
 
 module.exports = router;
