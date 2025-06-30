d2f44eef8e61bca021ff11a35bd7024968ed8028
CreativeMarkus <mark@creativemarkus.com>
2025-06-30 14:17:23 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 const passport = require("passport");
 const { User } = require("../models.js");
 check('Username', 'Username contains non-alphanumeric characters - not allowed.').isAlphanumeric()
