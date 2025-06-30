5eb6fef6ab48375c4579bb5a5a9992deea14f831
CreativeMarkus <mark@creativemarkus.com>
2025-06-30 09:46:49 -0400

    update

--- auth.js
+++ auth.js
@@ -1,19 +1,22 @@
-const jwtSecret = 'your_jwt_secret';
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
+const jwtSecret = 'your_jwt_secret'; 
 
-require('./passport');
+const jwt = require('jsonwebtoken'),
+      passport = require('passport');
+
+require('./passport'); 
+
 
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.Username,
-    expiresIn: '7d',
-    algorithm: 'HS256'
+    subject: user.Username, 
+    expiresIn: '7d',        
+    algorithm: 'HS256'      
   });
 };
 
-module.exports = (app) => {
-  app.post('/login', (req, res) => {
+
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
@@ -21,10 +24,12 @@ module.exports = (app) => {
           user: user
         });
       }
+
       req.login(user, { session: false }, (error) => {
         if (error) {
-          return res.send(error);
+          res.send(error);
         }
+
         let token = generateJWTToken(user.toJSON());
         return res.json({ user, token });
       });
