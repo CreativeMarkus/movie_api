6f9cbae9d397f3f56721fed96db0c32330afd28d
CreativeMarkus <mark@creativemarkus.com>
2025-06-30 09:49:54 -0400

    fixed the password file

--- passport.js
+++ passport.js
@@ -1,7 +1,8 @@
 const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   Models = require('./models.js'),
-  passportJWT = require('passport-jwt');
+  passportJWT = require('passport-jwt'),
+  bcrypt = require('bcrypt');
 
 let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
@@ -14,38 +15,37 @@ passport.use(
       passwordField: 'Password',
     },
     async (username, password, callback) => {
-      console.log(`${username} ${password}`);
-      await Users.findOne({ Username: username })
-      .then((user) => {
+      try {
+        const user = await Users.findOne({ Username: username });
+
         if (!user) {
-          console.log('incorrect username');
+          console.log('Incorrect username');
           return callback(null, false, {
             message: 'Incorrect username or password.',
           });
         }
-        console.log('finished');
-        return callback(null, user);
-      })
-      .catch((error) => {
-        if (error) {
-          console.log(error);
-          return callback(error);
+
+        const isValidPassword = await bcrypt.compare(password, user.Password);
+
+        if (!isValidPassword) {
+          console.log('Incorrect password');
+          return callback(null, false, {
+            message: 'Incorrect username or password.',
+          });
         }
-      })
+
+        console.log('Authentication successful');
+        return callback(null, user);
+      } catch (error) {
+        console.log('Error during authentication:', error);
+        return callback(error);
+      }
     }
   )
 );
 
-
-passport.use(new JWTStrategy({
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
-}, async (jwtPayload, callback) => {
-  return await Users.findById(jwtPayload._id)
-    .then((user) => {
-      return callback(null, user);
-    })
-    .catch((error) => {
-      return callback(error)
-    });
-}));
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: proce
