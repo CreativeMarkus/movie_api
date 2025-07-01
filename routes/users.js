596d21b085de699a60c67fc26d8096b1f0664f9d
CreativeMarkus <mark@creativemarkus.com>
2025-07-01 11:21:31 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,17 @@
-const express = require("express");
-const router = express.Router();
-const { check, validationResult } = require("express-validator");
-const { User } = require("../models.js");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-router.post(
-  "/",
-  [
-    check("Username", "Username is required").isLength({ min: 5 }),
-    check("Username", "Username contains non-alphanumeric characters - not allowed.")
-      .isAlphanumeric(),
-    check("Password", "Password is required").not().isEmpty(),
-    check("Email", "Email does not appear to be valid").isEmail(),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    }
+const userSchema = new mongoose.Schema({
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+});
 
-    const hashedPassword = User.hashPassword(req.body.Password);
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hashSync(password, 10);
+};
 
-    try {
-      const existingUser = await User.findOne({ Username: req.body.Username });
-      if (existingUser) {
-        return res.status(400).send(req.body.Username + " already exists");
-      }
+const User = mongoose.model('User', userSchema);
 
-      const newUser = await User.create({
-        Username: req.body.Username,
-        Password: hashedPassword,
-        Email: req.body.Email,
-        Birthday: req.body.Birthday,
-      });
-
-      // Sanitize response
-      const userObj = newUser.toObject();
-      delete userObj.Password;
-
-      res.status(201).json(userObj);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Error: " + error);
-    }
-  }
-);
-
-module.exports = router;
+module.exports = { User };
