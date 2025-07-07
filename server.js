e02664fca305454642c54c2c72bbeb49e7d7070e
CreativeMarkus <mark@creativemarkus.com>
2025-07-07 12:42:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -7,10 +6,9 @@ const cors = require('cors');
 
 const app = express();
 
-
 app.use(morgan('common'));
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
@@ -23,10 +21,11 @@ mongoose.connection.once('open', () => {
   console.log('MongoDB connected successfully!');
 });
 
-
 const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
 
+const moviesRouter = require('./routes/movies');
+app.use('/movies', moviesRouter); 
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
