b8f1faa79b9f37e5d5c82e4a54ea311bd5ec3996
CreativeMarkus <mark@creativemarkus.com>
2025-07-21 10:58:20 -0400

    Prepare for Heroku deploy

--- models/movies.js
+++ models/movies.js
@@ -1,27 +1,11 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  director: {
-    type: String,
-    trim: true
-  },
-  year: {
-    type: Number,
-    min: 1800 
-  },
-  genre: {
-    type: String,
-    trim: true
-  },
-  description: {
-    type: String,
-    trim: true
-  }
+  title: { type: String, required: true, trim: true },
+  director: { type: String, trim: true },
+  year: { type: Number, min: 1800 },
+  genre: { type: String, trim: true },
+  description: { type: String, trim: true }
 }, {
   timestamps: true
 });
--- routes/movies.js
+++ routes/movies.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const { Movie } = require('../models.js');
+const Movie = require('../models/movie');
 
 const router = express.Router();
 
-// GET all movies
 router.get('/', async (req, res) => {
   try {
     const movies = await Movie.find();
     res.json(movies);
   } catch (err) {
-    res.status(500).send(err);
+    console.error('Error in /movies route:', err);
+    res.status(500).send('Server error');
   }
 });
 
