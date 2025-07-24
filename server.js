d176093f8be8b6ae44ce5018c25f7be194e7ce1c
CreativeMarkus <mark@creativemarkus.com>
2025-07-24 15:17:34 -0400

    updated the files

--- server.js
+++ server.js
@@ -1,32 +1,75 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const morgan = require('morgan');
 const cors = require('cors');
+
 
 const app = express();
 
-app.use(morgan('common'));
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect(process.env.CONNECTION_URI, {
+mongoose.connect(process.env.CONNECTION_URI || 'mongodb://localhost:27017/movieDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const allowedOrigins = [
+  'http://localhost:1234',
+  'http://localhost:3000',
+
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this application does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+
+const Movie = mongoose.model('Movie', new mongoose.Schema({
+  title: String,
+  description: String,
+  genre: {
+    name: String,
+    description: String
+  },
+  director: {
+    name: String,
+    bio: String,
+    birthyear: String
+  },
+  imageUrl: String,
+  featured: Boolean
+}));
+
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.status(200).json(movies);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
-mongoose.connection.on('error', err => console.error('MongoDB error:', err));
-mongoose.connection.once('open', () => console.log('MongoDB connected successfully!'));
 
-const usersRouter = require('./routes/users');
-const moviesRouter = require('./routes/movies');
-
-
-app.use('/users', usersRouter);
-app.use('/movies', moviesRouter);
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the Movie API!');
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app; 
