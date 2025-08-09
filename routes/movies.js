4b703429aa23d48dd2fd032949e446655d725b8f
CreativeMarkus <mark@creativemarkus.com>
2025-08-09 00:13:45 -0400

    Fix movie route and model imports

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Movie } = require('../models.js');
+const Movie = require('../models/movies');
 
 router.get('/', async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -5,52 +5,31 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
-const { Movie, User } = require('./models');
 const usersRoutes = require('./routes/users.js');
+const moviesRoutes = require('./routes/movies.js');
+const genresRoutes = require('./routes/genres.js');
+const directorsRoutes = require('./routes/directors.js');
 
 const app = express();
 
-
-mongoose.connect(process.env.CONNECTION_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
-
 
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
+app.use(express.static('public'));
 
 app.use('/users', usersRoutes);
-
-
-app.get('/movies', async (req, res) => {
-  try {
-    const movies = await Movie.find();
-    res.status(200).json(movies);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error: ' + err);
-  }
-});
-
-app.get('/movies/:title', async (req, res) => {
-  try {
-    const movie = await Movie.findOne({ Title: req.params.title });
-    if (!movie) return res.status(404).send('Movie not found');
-    res.status(200).json(movie);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error: ' + err);
-  }
-});
-
+app.use('/movies', moviesRoutes);
+app.use('/genres', genresRoutes);
+app.use('/directors', directorsRoutes);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the public Movie API!');
 });
-
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
