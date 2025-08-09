a66fb5c9a2ca26fae2cdf3855f935ffade8c8d7b
CreativeMarkus <mark@creativemarkus.com>
2025-08-08 23:14:44 -0400

    Prepare backend for Heroku

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -6,26 +5,26 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
-const { Movie, User } = require('./models'); // Your Mongoose models
+const { Movie, User } = require('./models');
 const usersRoutes = require('./routes/users.js');
 
 const app = express();
 
-// Connect to MongoDB
+
 mongoose.connect(process.env.CONNECTION_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// Middleware
+
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-// Routes
+
 app.use('/users', usersRoutes);
 
-// Public movies routes without authentication
+
 app.get('/movies', async (req, res) => {
   try {
     const movies = await Movie.find();
@@ -47,12 +46,12 @@ app.get('/movies/:title', async (req, res) => {
   }
 });
 
-// Root route to confirm server is running
+
 app.get('/', (req, res) => {
   res.send('Welcome to the public Movie API!');
 });
 
-// Start server
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
