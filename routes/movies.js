6f3df2d9a94f663f49332217c405eb2fc989baa2
CreativeMarkus <mark@creativemarkus.com>
2025-08-11 16:28:18 -0400

    Update movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Movie = require('../models/movie.js');
+const Movie = require('../models/movies.js');
 
 router.get('/', (req, res) => {
   Movie.find()
