6477c7583d20950845e6250178499a15e0707daf
CreativeMarkus <mark@creativemarkus.com>
2025-08-11 15:31:34 -0400

    Make /movies public for Lesson 3.4

--- routes/movies.js
+++ routes/movies.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const Movies = require('../models.js').Movie;
 const router = express.Router();
-const Movie = require('../models/movie');
 
-router.get('/', async (req, res) => {
-  try {
-    const movies = await Movie.find();
-    res.json(movies);
-  } catch (error) {
-    res.status(500).send('Error fetching movies');
-  }
+router.get('/', (req, res) => {
+  Movies.find()
+    .then((movies) => res.json(movies))
+    .catch((err) => res.status(500).send('Error: ' + err));
+});
+
+router.get('/:Title', (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      if (!movie) {
+        return res.status(404).send('Movie not found');
+      }
+      res.json(movie);
+    })
+    .catch((err) => res.status(500).send('Error: ' + err));
 });
 
 module.exports = router;
