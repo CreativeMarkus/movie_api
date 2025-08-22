925fc417f81f7bad41365a3e2d603c791c10c9fa
CreativeMarkus <mark@creativemarkus.com>
2025-08-22 11:05:17 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+// Get all users
 router.get('/', async (req, res) => {
   try {
     const users = await User.find();
@@ -11,6 +14,7 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Get user by ID
 router.get('/:id', async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
@@ -24,9 +28,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// Create new user with hashed password
 router.post('/', async (req, res) => {
   try {
-    const newUser = new User(req.body);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      ...req.body,
+      password: hashedPassword
+    });
     await newUser.save();
     res.status(201).json(newUser);
   } catch (err) {
@@ -34,6 +43,24 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Login route
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return res.status(401).send('Invalid username or password');
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).send('Invalid username or password');
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ message: 'Login successful', token });
+  } catch (err) {
+    res.status(500).send('Error: ' + err);
+  }
+});
+
+// Update user
 router.put('/:id', async (req, res) => {
   try {
     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
@@ -47,6 +74,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+// Delete user
 router.delete('/:id', async (req, res) => {
   try {
     const deletedUser = await User.findByIdAndDelete(req.params.id);
@@ -60,4 +88,4 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-module.exports = router; 
+module.exports = router;
