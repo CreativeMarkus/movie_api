76bb753548016ef34773c3ca7df619755fcc1414
CreativeMarkus <mark@creativemarkus.com>
2025-09-02 05:49:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const authRoutes = require('./auth.js');
 const usersRoutes = require('./routes/users.js');
 const moviesRoutes = require('./routes/movies.js');
 const genresRoutes = require('./routes/genres.js');
@@ -13,26 +14,22 @@ const directorsRoutes = require('./routes/directors.js');
 
 const app = express();
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// Middleware
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// CORS configuration
 app.use(cors({
-  origin: 'http://localhost:3000', // React frontend URL
-  credentials: true // Allow cookies / auth headers
+  origin: 'http://localhost:3000',
+  credentials: true
 }));
 
-// Serve static files
 app.use(express.static('public'));
 
-// Routes
+app.use('/users', authRoutes);
 app.use('/users', usersRoutes);
 app.use('/movies', moviesRoutes);
 app.use('/genres', genresRoutes);
@@ -42,7 +39,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to the public Movie API!');
 });
 
-// Start server
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
   console.log('Listening on Port ' + port);
